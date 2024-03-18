6c666016b8a729994cc0c32377e649f0e1a8d167
Mengxuan Wu <mwu171@xe-10.cs.ucr.edu>
2024-03-17 23:27:34 -0700

    add supply request trigger

--- sql/src/create_trigger.sql
+++ sql/src/create_trigger.sql
@@ -1,4 +1,5 @@
 DROP TRIGGER IF EXISTS updateunits ON orders;
+DROP TRIGGER IF EXISTS updatesupply ON ProductSupplyRequests;
 
 CREATE OR REPLACE FUNCTION update_product_units()
 RETURNS TRIGGER AS $$
@@ -14,3 +15,21 @@ CREATE TRIGGER updateunits
 AFTER INSERT ON orders 
 FOR EACH ROW
 EXECUTE PROCEDURE update_product_units();
+
+CREATE OR REPLACE FUNCTION update_supply_request()
+RETURNS TRIGGER AS $$
+BEGIN
+    UPDATE Product 
+    SET numberOfUnits = numberOfUnits + NEW.unitsRequested 
+    WHERE storeID = NEW.storeid AND productName = NEW.productName;
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER updatesupply 
+AFTER INSERT ON ProductSupplyRequests 
+FOR EACH ROW
+EXECUTE PROCEDURE update_supply_request();
+
+
+
